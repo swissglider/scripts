0348d61195a7e77ed84c457e13087277b2de02b6
swissglider <npm@mailschweiz.com>
2018-06-21 09:55:51 +0200

    Create install.sh

---
+++ xps/install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# how to use this Script:
+# 
+
+#update system
+sudo apt update -y && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove -y
+
+#BIOS update
+sudo fwupdmgr get-devices
+sudo fwupdmgr refresh
+sudo fwupdmgr get-updates
+sudo fwupdmgr update
+
+
+#uninstall unused apps
+sudo apt remove -y --purge evince* thunderbird chromium-browser libreoffice* aisleriot webbrowser-app rhythmbox shotwell*
+sudo apt remove -y --purge gnome-mahjongg gnome-sudoku gnome-calendar gnome-mines
+sudo apt -y autoremove
+sudo apt clean
+
+#install additionals
+sudo apt install -y openssh-server
+
+#update chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo apt update
+sudo apt install google-chrome-stable
+sudo apt --only-upgrade install google-chrome-stable
