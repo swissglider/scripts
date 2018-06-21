76fcecbf811595d93b60a85f4f1512f3407444e6
swissglider <npm@mailschweiz.com>
2018-06-21 16:57:21 +0200

    Create install_dev_tools.sh

---
+++ xps/install_dev_tools.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# how to use this Script:
+# wget -O - https://git.io/fCxqS | bash
+
+#install development environment
+# ToDo:
+# install visual code studio
+# install vcs plugins and configuration
+# install firefox and configure as developer
+# install docker
+# install docker compose
+# install python docker env
+# install node docker env
+# ev. install virtualbox
