b46370178a037354fd21d61c8c9d93a9a0816356
diener <guido@diener.mobi>
2018-06-23 13:49:49 +0200

    new vsc addon --> yzhang.markdown-all-in-one

--- computer/setup/xps/install_dev_tools.sh
+++ computer/setup/xps/install_dev_tools.sh
@@ -44,6 +44,7 @@ code --install-extension ms-python.python
 code --install-extension tht13.rst-vscode
 code --install-extension azaugg.vscode-python-docstring
 code --install-extension ms-python.python
+code --install-extension yzhang.markdown-all-in-one
 
 /usr/bin/python2.7 -m pip install -U pylint --user
  /usr/bin/python3.6 -m pip install -U pylint --user
