29173dfdd3254ba044af75e827dd3a9d881f4c74
swissglider <npm@mailschweiz.com>
2018-12-17 20:36:15 +0100

    Update iobroker.sh

--- computer/setup/test-server ubuntu/iobroker.sh
+++ computer/setup/test-server ubuntu/iobroker.sh
@@ -2,19 +2,15 @@
 ## install iobroker on debian
 ## http://www.iobroker.net/docu/?page_id=5106&lang=de
 
-##-----------------------------------------------------------------------
-## Install Node --> see new Version on https://nodejs.org/en/
-##-----------------------------------------------------------------------
+## node nodejs npm
 # Using as root
-curl -sL https://deb.nodesource.com/setup_11.x | bash -
-apt install -y build-essential libavahi-compat-libdnssd-dev libudev-dev libpam0g-dev nodejs
-# apt install -y avahi-daemon avahi-discover avahi-utils libnss-mdns mdns-scan
+apt --purge remove node
+apt --purge remove nodejs
+apt autoremove
 reboot
 
-mkdir /opt/iobroker
-chmod 777 /opt/iobroker
-cd /opt/iobroker
-sudo npm install iobroker --unsafe-perm
+# install ioBroker and update it
+curl -sL https://raw.githubusercontent.com/ioBroker/ioBroker/stable-installer/installer.sh | bash -
 systemctl daemon-reload
 systemctl enable iobroker
 systemctl start iobroker
@@ -22,12 +18,8 @@ systemctl start iobroker
 iobroker update
 iobroker upgrade
 
-##-----------------------------------------------------------------------
-## install the following adapter:
-## - 
-iobroker add influxdb --host test
-iobroker add node-red --host test  (node-red-contrib-huemagic)
-# iobroker add hue  --host test
-
-
-systemctl restart iobroker
+# Configure the master:
+# http://www.iobroker.net/docu/?page_id=3068&lang=de
+iobroker multihost enable
+#iobroker multihost browse
+#sudo iobroker multihost connect
