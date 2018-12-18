b77ad4a6da36974ca6d19864bc3364000d2cec90
swissglider <npm@mailschweiz.com>
2018-12-18 07:26:16 +0100

    Update grafana.sh

--- computer/setup/test-server ubuntu/grafana.sh
+++ computer/setup/test-server ubuntu/grafana.sh
@@ -4,9 +4,9 @@
 
 # operate as su
 
-wget https://dl.grafana.com/oss/release/grafana_5.4.0_amd64.deb
+wget https://dl.grafana.com/oss/release/grafana_5.4.2_amd64.deb
 apt install -y adduser libfontconfig
-dpkg -i grafana_5.4.0_amd64.deb
+dpkg -i grafana_5.4.2_amd64.deb
 
 systemctl daemon-reload
 systemctl start grafana-server
