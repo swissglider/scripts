248c69d4c8a35fec80cd6dac5f6c22a3127b0726
swissglider <npm@mailschweiz.com>
2019-04-05 21:30:34 +0200

    Update iobroker.sh

--- computer/setup/test-server ubuntu/iobroker.sh
+++ computer/setup/test-server ubuntu/iobroker.sh
@@ -9,6 +9,10 @@ apt --purge remove nodejs
 apt autoremove
 reboot
 
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y build-essential libavahi-compat-libdnssd-dev libudev-dev libpam0g-dev nodejs
+reboot
+
 # install ioBroker and update it
 curl -sL https://raw.githubusercontent.com/ioBroker/ioBroker/stable-installer/installer.sh | bash -
 systemctl daemon-reload
