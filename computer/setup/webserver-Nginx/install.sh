719b93e28d555a1fb79b229da3d679653c0f0319
swissglider <npm@mailschweiz.com>
2020-04-01 17:31:37 +0200

    Update install.sh

--- computer/setup/webserver-Nginx/install.sh
+++ computer/setup/webserver-Nginx/install.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-apt update -y
-apt upgrade -y
+# change to local user (not root)
 
+sudo apt update -y
+sudo apt upgrade -y
+
+sudo apt install nginx -y
+
+sudo mkdir -p /var/www/homeoverview/html
+sudo chown -R $USER:$USER /var/www/homeoverview/html
+sudo chmod -R 755 /var/www/homeoverview
+
+echo HomeOverview > /var/www/homeoverview/html/index.html
+
+sudo sed -i 's/# server_names_hash_bucket_size 64;/server_names_hash_bucket_size 64;/g' /etc/nginx/nginx.conf
+sudo systemctl restart nginx
