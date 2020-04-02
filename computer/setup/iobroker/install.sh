a1f3ea38fcca737651c6b1b3e8ec65f31c678281
swissglider <npm@mailschweiz.com>
2020-04-02 14:37:23 +0200

    Create install.sh

---
+++ computer/setup/iobroker/install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+cd
+sudo apt update -y
+sudo apt upgrade -y
+
+#node.js
+#######################################################################
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+#redis
+#######################################################################
+sudo apt-get install redis-server -y
+# vi /etc/redis/redis.conf
+# set bind 0.0.0.0
+
+#iobroker
+#######################################################################
+sudo apt-get install -y build-essential libavahi-compat-libdnssd-dev libudev-dev libpam0g-dev
+curl -sL https://iobroker.net/install.sh | bash -
+mkdir /opt/iobroker/backups
+
+iobroker stop
+iobroker update
+iobroker update all
+iobroker update self
+iobroker upgrade
+iobroker upgrade all
+iobroker upgrade self
+iobroker start
+
+iobroker setup custom
+# Current configuration:
+# - Objects database:
+#   - Type: file
+#   - Host/Unix Socket: 0.0.0.0
+#   - Port: 9001
+# - States database:
+#   - Type: redis
+#   - Host/Unix Socket: 0.0.0.0
+#   - Port: 6379
+# - Data Directory: ../../iobroker-data/
