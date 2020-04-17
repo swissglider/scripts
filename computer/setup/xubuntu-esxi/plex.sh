d70a0087817e515521008686db5dafdfbd9efc71
swissglider <npm@mailschweiz.com>
2020-04-17 16:07:45 +0200

    Create plex.sh

---
+++ computer/setup/xubuntu-esxi/plex.sh
@@ -0,0 +1,7 @@
+# get newest release of plex
+# https://www.plex.tv/media-server-downloads/#plex-media-server
+get https://downloads.plex.tv/plex-media-server-new/1.19.1.2645-ccb6eb67e/debian/plexmediaserver_1.19.1.2645-ccb6eb67e_amd64.deb
+
+#install https://support.plex.tv/articles/200288586-installation/
+
+sudo dpkg -i plexmediaserver_1.19.1.2645-ccb6eb67e_amd64.deb
