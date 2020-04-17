7b2a7c7a2d3fdfec1c31a1270128a29e6473ee9a
swissglider <npm@mailschweiz.com>
2020-04-17 18:52:44 +0200

    Update jdownloader.sh

--- computer/setup/xubuntu-esxi/jdownloader.sh
+++ computer/setup/xubuntu-esxi/jdownloader.sh
@@ -3,3 +3,8 @@ sudo apt upgrade -y
 
 sudo apt install snapd -y
 sudo snap install jdownloader2
+
+# mount samba share
+sudo apt-get install cifs-utils keyutils -y
+sudo mkdir /mnt/external
+# add the following to the /etc/fstab
