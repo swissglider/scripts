2d6479e6ddad0a11d8a37ebfa3ff3cd755ab8181
swissglider <npm@mailschweiz.com>
2020-04-18 06:06:57 +0200

    Update install.sh

--- computer/setup/xubuntu-esxi/install.sh
+++ computer/setup/xubuntu-esxi/install.sh
@@ -18,3 +18,12 @@ sudo apt install dconf-editor -y
 
 # Under the Unit section of
 # /etc/systemd/system/multi-user.target.wants/open-vm-tools.service
+
+# install conky
+sudo apt-get install conky-all -y
+cp /etc/conky/conky.conf .conkyrc
+sudo vi /etc/xdg/autostart/conky.desktop
+# [Desktop Entry]
+# Type=Application
+# Name=Conky
+# Exec=conky
